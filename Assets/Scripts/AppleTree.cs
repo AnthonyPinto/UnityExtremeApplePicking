d22529f355bfd1f0be7f780115dda66bdfe61993
Anthony Pinto <AnthonyPinto@users.noreply.github.com>
2023-01-07 09:33:30 -0800

    Rename Tree > AppleTree to avoid name collision

--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tree : MonoBehaviour
+public class AppleTree : MonoBehaviour
 {
     public GameObject ApplePrefab;
     public Transform AppleSpawnPoint;
